636a61c581daaf7d7abacb01ba55e2ff7e1cde4c
rickysugiarta <sugantric@yahoo.com>
2024-09-30 22:50:59 +1000

    add slug related function to car

--- cake/src/Model/Entity/Car.php
+++ cake/src/Model/Entity/Car.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Utility\Text;
 
 class Car extends Entity
 {
@@ -16,4 +17,17 @@ class Car extends Entity
         'created' => true,
         'modified' => true,
     ];
+
+    protected function _getSlug()
+    {
+        return Text::slug(strtolower($this->license_state . $this->license_plate));
+    }
+
+    protected function _setSlug($slug)
+    {
+        $this->license_state = substr($slug,0,3);
+        $this->license_plate = substr($slug,3);
+
+        return $slug;
+    }
 }
