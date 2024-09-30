4382dd580fedb173fc79eb7d2808f9f3a180d811
rickysugiarta <sugantric@yahoo.com>
2024-09-30 23:00:41 +1000

    fix typo and add quotes sample insert data

--- mysql/init.sql
+++ mysql/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE cars (
     license_state VARCHAR(3) NOT NULL,
     vin VARCHAR(50) NULL,
     year VARCHAR(4) NULL,
-    colour VARCHAR(20)) NULL,
+    colour VARCHAR(20) NULL,
     make VARCHAR(20) NULL,
     model VARCHAR(20) NULL,
     created DATETIME,
@@ -19,14 +19,17 @@ CREATE TABLE quotes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     license_plate VARCHAR(20) NOT NULL,
     license_state VARCHAR(3) NOT NULL,
-    price INT NOT NULL,
-    repairer VARCHAR(255) NOT NULL,
-    overviewOfWork VARCHAR(191) NOT NULL,
+    price decimal NOT NULL,
+    repairer VARCHAR(50) NOT NULL,
+    overview_of_work VARCHAR(200) NOT NULL,
     created DATETIME,
     modified DATETIME,
-    FOREIGN KEY fk_car (license_plate, license_state) REFERENCES car(license_plate, license_state)
+    FOREIGN KEY fk_car (license_plate, license_state) REFERENCES cars(license_plate, license_state)
 ) 
 
 INSERT INTO cars
 VALUES
 ('DTR123', 'NSW', '5TDBW5G16BS451999', '2024', 'Black', 'Mazda', 'CX-3', NOW(), NOW());
+
+
+insert into quotes (license_plate, license_state, price, repairer, overview_of_work, created, modified) values ('DTR123', 'NSW', 850, 'Jake', '4x tire change', NOW(), NOW());
